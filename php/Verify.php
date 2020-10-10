a5ff1fff078b70f91a0fd79985a247d915f241d3
Bhadresh Malankiya <64741307+MB-Devlopers@users.noreply.github.com>
2020-10-10 11:08:37 +0530

    updated at 10 october 2020
    make shyor put it in xampp and bydefault run apache and mysql

---
+++ php/Verify.php
@@ -0,0 +1,43 @@
+<?php
+	$username=$_POST["uname"];
+	$password=$_POST["password"];
+
+	$query = " Select * from users where username = '$username' and password= '$password'" ;
+
+	$con = mysqli_connect("localhost","root","","demo");
+	if(!$con)
+	{
+		die("Connt connet to Database");
+	}
+	else
+	{	
+	
+		if($result = mysqli_query($con, $query))
+		{	
+			
+			if(mysqli_num_rows($result) > 0)
+			{	
+				
+				
+				while($row = mysqli_fetch_array($result)){
+					
+					session_start();
+					header('location: ../addproducts.html');
+					
+				}
+			}
+			else
+			{
+				echo "Incorrect username or pasword ";
+				echo "Redirect in 3 Seconds";	
+				header('refresh: 3; url=../login.html');
+			
+			}
+		}
+		else{
+		  mysqli_close($con);;
+		}
+	}
+		
+
+?>
